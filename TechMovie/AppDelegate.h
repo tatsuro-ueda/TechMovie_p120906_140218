4def894005790719c53c86101fd867f4aa870766
weed <weed_7777@yahoo.co.jp>
2012-09-06 05:34:14 +0900

    Initial Commit

---
+++ TechMovie/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TechMovie
+//
+//  Created by Tatsuro Ueda on 12/09/06.
+//  Copyright (c) 2012年 Tatsuro Ueda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TechMovie/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TechMovie
+//
+//  Created by Tatsuro Ueda on 12/09/06.
+//  Copyright (c) 2012年 Tatsuro Ueda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
